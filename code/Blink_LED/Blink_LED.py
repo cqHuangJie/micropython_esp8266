c1cac3a83626a8aaff2054838d5d9346f7c925b0
cqHuangJie <104741541+cqHuangJie@users.noreply.github.com>
2022-05-03 09:45:51 +0800

    Update Blink_LED.py

--- code/Blink_LED/Blink_LED.py
+++ code/Blink_LED/Blink_LED.py
@@ -1,10 +1,10 @@
 # Complete project details at https://RandomNerdTutorials.com
 
-from machine import Pin
-from time import sleep
+from machine import Pin#导入引脚类
+from time import sleep#导入延时类
 
-led = Pin(2, Pin.OUT)
+led = Pin(2, Pin.OUT)#实例化引脚为输出模式
 
-while True:
-  led.value(not led.value())
-  sleep(0.5)
+while True:#无限循环
+  led.value(not led.value())#引脚电平反置，控制小灯亮、灭
+  sleep(0.5)#延时0.5秒
